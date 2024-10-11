64bbc7ba0d78a680e1b2c79610f983022b5c13d2
Alexey Kholyavski <kholyavski@mail.ru>
2024-10-11 17:19:04 +0300

    add and update authentication

---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'max:255'],
+            'email' => ['required', 'email', 'unique:users'],
+            'password' => ['required', 'min:7', 'confirmed'],
+        ];
+    }
+}
---
+++ app/Http/Requests/Post/UpdateRequest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Requests\Post;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'string', 'max:35'],
+            'body' => 'required',
+        ];
+    }
+}
